afee6b07ed2c25fe4f5467194778f8ea0bdc1b8f
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-12-03 02:25:08 +1000

    Add skeleton modtool command

---
+++ Modtool/modtool.py
@@ -0,0 +1,12 @@
+"""
+Created on Dec 3, 2023
+
+@author: CyberiaResurrection
+"""
+import logging
+
+logger = logging.getLogger('Modtool')
+
+
+if __name__ == '__main__':
+    logger.error("And the question is what is the question?")
