e4601541db082eecc21547d0a968b896c495aed4
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-12-05 00:17:53 +1000

    Check that single-quotes are encoded during XML read

--- Modtool/ModFileReader.py
+++ Modtool/ModFileReader.py
@@ -6,7 +6,12 @@ Created on Dec 4, 2023
 Encapsulate all the mod-XML-file gubbins here, as a single point of truth on the way in from and out to the
 raw XML, so no one else has to worry
 """
+from defusedxml.ElementTree import parse
 
 
 class ModFileReader(object):
-    pass
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.modfile = parse(self.filename)
+        assert self.modfile is not None, "XML not parsed"
---
+++ Tests/testmodfilereader.py
@@ -0,0 +1,27 @@
+import unittest
+
+from Modtool.ModFileReader import ModFileReader
+from Tests.baseTest import baseTest
+from xml.sax.saxutils import escape, unescape
+
+
+class testModFileReader(baseTest):
+    def test_check_strname_encoded_in_xml_file_after_parsing(self):
+        filename = self.unpack_filename('sample_files/frankies_wild_ride/treasuretable.xml')
+        escape_dir = {'\'': '&apos;'}
+        escape_reverse = {'&apos;': '\''}
+
+        foo = ModFileReader(filename)
+        root = foo.modfile.getroot()
+        # dig out strName fields and check they have been htmlencoded
+        for child in root.iter('column'):
+            if 'strName' == child.attrib['name']:
+                child_text = child.text
+                returned = unescape(child_text, escape_reverse)
+                remix_text = escape(returned, escape_dir)
+
+                self.assertEqual(remix_text, child_text, "strName content should be escaped after read-in")
+
+
+if __name__ == '__main__':
+    unittest.main()
