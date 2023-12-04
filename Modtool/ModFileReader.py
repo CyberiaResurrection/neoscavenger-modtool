7b79b563a845006d3f14b28baf1a3f197f4f7a44
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-12-04 22:48:33 +1000

    Add stub ModFileReader

---
+++ Modtool/ModFileReader.py
@@ -0,0 +1,12 @@
+"""
+Created on Dec 4, 2023
+
+@author: CyberiaResurrection
+
+Encapsulate all the mod-XML-file gubbins here, as a single point of truth on the way in from and out to the
+raw XML, so no one else has to worry
+"""
+
+
+class ModFileReader(object):
+    pass
